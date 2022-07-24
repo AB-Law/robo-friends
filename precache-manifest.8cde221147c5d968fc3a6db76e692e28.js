1e0336f05ae502a3c693224baf47ba28c492f392
Akshay B <akshay.law35@gmail.com>
2022-07-24 22:11:39 +0530

    Updates

---
+++ precache-manifest.8cde221147c5d968fc3a6db76e692e28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2e82dfba476d0a9eb5",
+    "url": "/robo-friends/static/css/main.a61d2cad.chunk.css"
+  },
+  {
+    "revision": "2f2e82dfba476d0a9eb5",
+    "url": "/robo-friends/static/js/main.2f2e82df.chunk.js"
+  },
+  {
+    "revision": "5795cdf317734256ec6e",
+    "url": "/robo-friends/static/css/1.acf35371.chunk.css"
+  },
+  {
+    "revision": "5795cdf317734256ec6e",
+    "url": "/robo-friends/static/js/1.5795cdf3.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo-friends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a212ed2ee5ab1158bc1c4a623b68c199",
+    "url": "/robo-friends/index.html"
+  }
+];
